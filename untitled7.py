26505a5548f31f137d3ffaaf59c0f9df28383ee0
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-07 23:20:15 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -11,7 +11,10 @@ if browse==True:
     if filename is None:
         st.write("Please upload a file")
     else:
-        with sr.AudioFile(filename) as source:
+        filename = AudioSegment.from_wav(filename)
+        wav.export("extracted.wav",format="wav")
+        audio_bytes = open("extracted.wav",'rb').read()
+        with sr.AudioFile(extracted.wav) as source:
              st.write("Done")
              audio_data = r.record(source)
              text = r.recognize_google(audio_data)
