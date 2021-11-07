616a2ccbab401b462e6ee54903e706b89a22d217
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-07 22:48:09 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -3,11 +3,10 @@ import speech_recognition as sr
 st.title("Speech to text converter")
 st.header("Browse a file or start recordingcbgfnn")
 col1,col2 = st.columns(2)
-col1.button("Record")
-col2.button("Browse file")
+rec=col1.button("Record")
+browse=col2.button("Browse file")
 r = sr.Recognizer()
 filename="Welcome.wav"
-record = st.button("Record")
 with sr.AudioFile(filename) as source:
     audio_data = r.record(source)
     text = r.recognize_google(audio_data)
