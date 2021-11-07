bf3271732d48bb5d317da8b715409ccb7f0831d3
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-07 22:46:18 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import speech_recognition as sr
 st.title("Speech to text converter")
-st.header("Browse a file or start recording")
+st.header("Browse a file or start recordingcbgfnn")
+col1,col2 = st.columns(2)
+rec = col1.button("Record")
+browse = col2.button("Browse file")
 r = sr.Recognizer()
 filename="Welcome.wav"
 record = st.button("Record")
