22b215ca1ff60fd0c5b450075cd0c234cb57b10e
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-07 22:53:42 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import speech_recognition as sr
 st.title("Speech to text converter")
-st.header("Browse a file or start recordingcbgfnn")
+st.header("Browse a file or start recording")
 col1,col2 = st.columns(2)
 rec=col1.button("Record")
 browse=col2.button("Browse file")
@@ -10,6 +10,7 @@ if browse==True:
     filename = st.file_uploader("Upload Files",type=['wav','mp4'])
     if filename:
         with sr.AudioFile(filename) as source:
+             st.write("Done")
              audio_data = r.record(source)
              text = r.recognize_google(audio_data)
              st.write(text)
