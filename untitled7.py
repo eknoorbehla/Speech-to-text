431e8020482abdce9a65f20aeec00af6690ea7e3
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-08 00:03:17 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -4,7 +4,7 @@ from pydub import AudioSegment
 st.title("Speech to text converter")
 st.header("Browse a file or start recording")
 col1,col2 = st.columns(2)
-option=st.selectbox("Choose",['Record','Browse file'])
+option=st.selectbox("Choose",['Choose an option','Record','Browse file'])
 r = sr.Recognizer()
 if option=="Browse file":
     filename = st.file_uploader("Upload Files",type=['wav','mp4'])
@@ -19,4 +19,15 @@ if option=="Browse file":
              text = r.recognize_google(audio_data)
              st.write("Your text")
              st.success(text)
-
+elif option=="Record":
+    fs = 44100  # Sample rate
+    seconds = duration  # Duration of recording
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    write('output.wav', fs, myrecording)
+    filename = 'output.wav'
+    with sr.AudioFile(filename) as source:
+             audio_data = r.record(source)
+             text = r.recognize_google(audio_data)
+             st.write("Your text")
+             st.success(text)
