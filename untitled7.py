150dc4634e39b3f8e473d1534037360ca1279bdf
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-07 19:27:19 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -1,1 +1,8 @@
 import streamlit as st
+import speech_recognition as sr
+r = sr.Recognizer()
+filename="Welcome.wav"
+with sr.AudioFile(filename) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+    st.write(text)
