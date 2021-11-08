ce98a32e1fe1b8f1b255898d278de635b1c6534e
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-08 11:21:09 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -3,29 +3,17 @@ import speech_recognition as sr
 from pydub import AudioSegment
 
 st.title("Speech to text converter")
-st.header("Browse a file or start recording")
-col1,col2 = st.columns(2)
-option=st.selectbox("Choose",['Choose an option','Record','Browse file'])
+st.header("Browse a file to Convert")
 r = sr.Recognizer()
-if option=="Browse file":
-    filename = st.file_uploader("Upload Files",type=['wav','mp4'])
-    if filename is None:
-        st.write("Please upload a file")
-    else:
-        filename = AudioSegment.from_wav(filename)
-        filename.export("extracted.wav",format="wav")
-        filename="extracted.wav"
-        with sr.AudioFile(filename) as source:
-             audio_data = r.record(source)
-             text = r.recognize_google(audio_data)
-             st.write("Your text")
-             st.success(text)
-elif option=="Record":
-    wav = AudioSegment.from_wav(file)
-    wav.export("extracted.wav", format='wav')
+filename = st.file_uploader("Upload Files",type=['wav','mp4'])
+if filename is None:
+    st.write("Please upload a file")
+else:
+    filename = AudioSegment.from_wav(filename)
+    filename.export("extracted.wav",format="wav")
     filename="extracted.wav"
-    with sr.AudioFile(wf) as source:
-             audio_data = r.record(source)
-             text = r.recognize_google(audio_data)
-             st.write("Your text")
-             st.success(text)
+    with sr.AudioFile(filename) as source:
+         audio_data = r.record(source)
+         text = r.recognize_google(audio_data)
+         st.write("Your text")
+         st.success(text)
