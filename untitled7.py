038d6fee48d86a47eee311a6509d975a2a5b861e
eknoorbehla <68049068+eknoorbehla@users.noreply.github.com>
2021-11-08 10:53:22 +0530

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import speech_recognition as sr
 from pydub import AudioSegment
+import pyaudio
+import wave
 st.title("Speech to text converter")
 st.header("Browse a file or start recording")
 col1,col2 = st.columns(2)
@@ -20,13 +22,32 @@ if option=="Browse file":
              st.write("Your text")
              st.success(text)
 elif option=="Record":
-    fs = 44100  # Sample rate
-    seconds = 10  # Duration of recording
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-    sd.wait()  # Wait until recording is finished
-    write('output.wav', fs, myrecording)
-    filename = 'output.wav'
-    with sr.AudioFile(filename) as source:
+    filename = "recorded.wav"
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    channels = 1
+    sample_rate = 44100
+    record_seconds = 5
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,channels=channels,rate=sample_rate,input=True,output=True,frames_per_buffer=chunk)
+    frames = []
+    print("Recording...")
+    for i in range(int(44100 / chunk * record_seconds)):
+        data = stream.read(chunk)
+    # if you want to hear your voice while recording
+    # stream.write(data)
+        frames.append(data)
+    print("Finished recording.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(filename, "wb")
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(sample_rate)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+    with sr.AudioFile(wf) as source:
              audio_data = r.record(source)
              text = r.recognize_google(audio_data)
              st.write("Your text")
